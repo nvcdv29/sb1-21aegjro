d5d4e99e071510ce3955d56315738c2864b7697e
nvcdv29 <69216296+nvcdv29@users.noreply.github.com>
2024-12-21 14:08:37 +0100

    border radius fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'image': '0.5rem', // custom image border radius
+      },
+      height: {
+        'gallery-sm': '200px',
+        'gallery-md': '250px',
+        'gallery-lg': '300px',
+      },
+      maxHeight: {
+        'gallery': '50vh',
+      }
+    },
   },
   plugins: [],
 };
