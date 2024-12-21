9200efd489d5f08e185b9b83377216f29b6a52bd
nvcdv29 <69216296+nvcdv29@users.noreply.github.com>
2024-12-21 17:05:57 +0100

    test run auto update

--- src/mockData.ts
+++ src/mockData.ts
@@ -1,41 +1,38 @@
-import { ImageNode } from "./types";
-
-// Simulated image structure using Unsplash images
-import { ImageNode } from "../types";
+import { ImageNode } from '../types';
 
 export const mockImageStructure: ImageNode[] = [
   {
-    name: "Architecture",
-    path: "src/img/architecture",
-    type: "directory",
-    children: [
+    "name": "Architecture",
+    "path": "img/architecture",
+    "type": "directory",
+    "children": [
       {
-        name: "arc-img-1.jpg",
-        path: "src/img/architecture/arc-img-1.jpg",
-        type: "file",
+        "name": "arc-img-1.jpg",
+        "path": "img/architecture/arc-img-1.jpg",
+        "type": "file"
       },
       {
-        name: "second-image.jpg",
-        path: "src/img/architecture/second-image.jpg",
-        type: "file",
-      },
-    ],
+        "name": "second-image.jpg",
+        "path": "img/architecture/second-image.jpg",
+        "type": "file"
+      }
+    ]
   },
   {
-    name: "Nature",
-    path: "src/img/nature",
-    type: "directory",
-    children: [
+    "name": "Nature",
+    "path": "img/nature",
+    "type": "directory",
+    "children": [
       {
-        name: "img-n1.jpg",
-        path: "src/img/nature/img-n1.jpg",
-        type: "file",
+        "name": "img-n1.jpg",
+        "path": "img/nature/img-n1.jpg",
+        "type": "file"
       },
       {
-        name: "natura-x.jpg",
-        path: "src/img/nature/natura-x.jpg",
-        type: "file",
-      },
-    ],
-  },
+        "name": "natura-x.jpg",
+        "path": "img/nature/natura-x.jpg",
+        "type": "file"
+      }
+    ]
+  }
 ];
